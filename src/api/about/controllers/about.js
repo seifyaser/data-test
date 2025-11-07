51776890e12e479f185c395950e43516b4691c61
seifyaser <saifyasser386@gmail.com>
2025-11-07 22:39:11 +0200

    add about

--- src/api/about/controllers/about.js
+++ src/api/about/controllers/about.js
@@ -1,9 +1,52 @@
 'use strict';
 
-/**
- *  about controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+// المفاتيح اللي عايزين نحذفها
+const KEYS = ['createdAt', 'updatedAt', 'publishedAt', 'documentId'];
+
+// دالة تنظيف شاملة تمر على كل الكائنات بعمق
+function cleanTimestamps(obj) {
+  if (!obj || typeof obj !== 'object') return;
+
+  if (Array.isArray(obj)) {
+    obj.forEach(cleanTimestamps);
+    return;
+  }
+
+  // لو في attributes نحذف المفاتيح
+  if (obj.attributes && typeof obj.attributes === 'object') {
+    KEYS.forEach(k => delete obj.attributes[k]);
+    Object.values(obj.attributes).forEach(cleanTimestamps);
+  }
+
+  // لو في data (علاقات populate)
+  if (obj.data) cleanTimestamps(obj.data);
+
+  // نحذف المفاتيح من المستوى الأعلى كمان لو موجودة
+  Object.keys(obj).forEach(key => {
+    if (KEYS.includes(key)) {
+      delete obj[key];
+    } else {
+      cleanTimestamps(obj[key]);
+    }
+  });
+}
+
+module.exports = createCoreController('api::about.about', ({ strapi }) => ({
+
+  async find(ctx) {
+    strapi.log.info('✅ Deep Cleaner: about.find running');
+    const response = await super.find(ctx);
+    cleanTimestamps(response);
+    return response;
+  },
+
+  async findOne(ctx) {
+    strapi.log.info('✅ Deep Cleaner: about.findOne running');
+    const response = await super.findOne(ctx);
+    cleanTimestamps(response);
+    return response;
+  },
+
+}));
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -479,9 +479,7 @@ export interface ApiAboutAbout extends Struct.SingleTypeSchema {
     draftAndPublish: false;
   };
   attributes: {
-    blocks: Schema.Attribute.DynamicZone<
-      ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
-    >;
+    about: Schema.Attribute.JSON;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -489,7 +487,6 @@ export interface ApiAboutAbout extends Struct.SingleTypeSchema {
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::about.about'> &
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
-    title: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
